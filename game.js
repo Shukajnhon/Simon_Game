5a9e950dd5c3af964334de331df00ee3d76b3365
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-09-28 08:35:17 +0700

    add new features: check answer and reset game

--- game.js
+++ game.js
@@ -4,33 +4,85 @@ const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+// console.log('Game pattern: ', gamePattern)
+// console.log('User click pattern: ', userClickedPattern)
+
+var started = false;
+var level = 0;
+
 // Jquery
+
+
 
 // Detect buttons when click
 $(".btn").click(function (e) {
     let userChosenColor = $(this).attr("id"); // use Jquery
     // let userChosenColor = e.target.id;
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern)
+    // console.log(userClickedPattern)
 
     // click to play sound
     playSound(userChosenColor)
 
     animatePress(userChosenColor)
 
+
+    // index of the last answer
+    checkAnswer(userClickedPattern.length - 1)
+
 })
 
 
 // Detect a Keyboard key has been pressed at the first time
-$("body").keypress(function (e) {
-    nextSequence()
+$(document).keypress(function () {
+    if (!started) {
+        // h1 content change when keyboard press at the first time to start
+        $("#level-title").text('Level ' + level)
+
+        nextSequence()
+
+        started = true
+
+    }
 })
 
 
+// Check answer
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success")
 
+        if (userClickedPattern.length === gamePattern.length) {
+            // call NextSequence() after 1s delay
+            setTimeout(function () {
+                nextSequence()
+            }, 1000)
+        }
+    } else {
+        console.log("Wrong")
+        playSound('wrong');
+
+        $("body").addClass("game-over")
+        setTimeout(function () {
+            $("body").removeClass("game-over")
+        }, 200)
+
+        $("#level-title").text('Game Over, Press Any Key to Restart')
+
+        startOver()
+    }
+}
 
 // next Sequence func
 function nextSequence() {
+
+    // Inside nextSequence(), increase the level by 1 every time nextSequence() is called
+    level++;
+
+    // update the h1 with this change in the value of level.
+    $("#level-title").text("Level " + level);
+
+
     // random number between 0 and 3;
     let randomNumber = Math.floor(Math.random() * 4);
 
@@ -43,7 +95,9 @@ function nextSequence() {
 
 
     // Refactor the code in playSound(), it will work for both playing sound in nextSequence() and when the user clicks a button.
-    playSound(userChosenColor)
+    playSound(randomChosenColor)
+
+
 
 
 }
@@ -67,4 +121,15 @@ function animatePress(currentColor) {
 }
 
 
+
+// Start Over
+function startOver() {
+    // reset level
+    level = 0;
+    gamePattern = [];
+    started = false;
+
+    console.log('GamePattern: ', gamePattern)
+    console.log('Started: ', started)
+}
 
